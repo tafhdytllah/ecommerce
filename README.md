b316b161c0fbaea0a925577c369e1c7b98d38291
tafhdytllah <taufikhh.97@gmail.com>
2024-05-27 19:28:15 +0700

    update database.sql and readme.md

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-## Ecommerce App
+# Project Starter
+
+## System Requirements
+Before getting started, ensure your system meets the following requirements:
+- [java 17](https://www.oracle.com/java/technologies/javase/jdk17-archive-downloads.html) is installed on your machine.
+- [Maven](https://maven.apache.org/) is installed on your machine.
+- [MySql](https://dev.mysql.com/doc/mysql-installation-excerpt/5.7/en/) is installed on your machine.
+
+### Installation
+
+1. Clone the repository:
+
+    ```bash
+    $ git clone https://github.com/tafhdytllah/ecommerce.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```bash
+    $ cd ecommerce
+    ```
+3. Create database "ecommerce" into your machine and execute this script [database.sql](/database.sql)
+
+4. Run into local machine :
+
+    ```bash
+    $ mvn spring-boot:run
+    ```
+
+5. The Spring Boot application will be accessible at `http://localhost:8080`.
+
+## Endpoint
+
+#### Show All Data Transaction
+
+- **Metode:** [GET]
+- **Endpoint:** `/api/transactions`
+
+    #### Response
+  - **Status Code:** [200]
+  - **Response Body:**
+      ```json
+      {
+          "data": [
+            {
+              "id": 1,
+              "productID": "2",
+              "productName": "Kulkas",
+              "amount": "5",
+              "customerName": "Budi",
+              "status": 1,
+              "transactionDate": "2024-05-27 11:37:22",
+              "createBy": "admin",
+              "createOn": "2024-05-27 11:37:22"
+            }  
+          ],
+          "status": [
+            {
+              "id": 0,
+              "name": "SUCCESS"
+            }
+    ]
+      }
+      ```
--- database.sql
+++ database.sql
@@ -1,67 +1,44 @@
-CREATE DATABASE ecommerce;
+# CREATE DATABASE ecommerce;
 
-USE ecommerce;
+# USE ecommerce;
 
-DROP TABLE transactions, products, customers;
+# DROP TABLE transactions, products, customers;
 # SHOW TABLES;
 # SELECT * FROM products;
 # SELECT * FROM customers;
 # SELECT * FROM transactions;
-# desc transactions;
+
 CREATE TABLE products(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         name VARCHAR(100) NOT NULL,
+                         created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                         updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 INSERT INTO products(name) VALUES('Laptop'),('Kulkas'),('Lemari'),('Mesin Cuci'),('Kompor');
 
 CREATE TABLE customers(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                          id INT AUTO_INCREMENT PRIMARY KEY,
+                          name VARCHAR(100) NOT NULL,
+                          created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                          updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 INSERT INTO customers(name) VALUES('Budi'),('Toni'),('Santi'),('Suci');
 
 CREATE TABLE transactions(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    product_id INT NOT NULL,
-    customer_id INT NOT NULL,
-    amount INT NOT NULL NOT NULL,
-    status BOOLEAN NOT NULL,
-    transaction_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    create_by VARCHAR(100) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (product_id) REFERENCES products(id),
-    FOREIGN KEY (customer_id) REFERENCES customers(id)
+                             id INT AUTO_INCREMENT PRIMARY KEY,
+                             product_id INT NOT NULL,
+                             customer_id INT NOT NULL,
+                             amount INT NOT NULL NOT NULL,
+                             status BOOLEAN NOT NULL,
+                             transaction_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+                             create_by VARCHAR(100) NOT NULL,
+                             created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                             updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                             FOREIGN KEY (product_id) REFERENCES products(id),
+                             FOREIGN KEY (customer_id) REFERENCES customers(id)
 );
 
 INSERT INTO transactions(product_id, customer_id, amount, status, create_by)
 VALUES (2,1,5,1, 'admin'), (1,1,3,1, 'admin'), (3,1,1,0,'admin'), (3,3,10,1,'admin');
-
-
-select
-    t.id,
-    t.product_id,
-    p.name as product_name,
-    t.amount,
-    c.name as customer_name,
-    t.status,
-    t.transaction_date,
-    t.create_by,
-    t.created_at
-from
-    transactions as t
-join
-    products as p on t.product_id = p.id
-join
-    customers as c on t.customer_id = c.id;
-
-
-truncate table transactions;
-
-select * from transactions;
